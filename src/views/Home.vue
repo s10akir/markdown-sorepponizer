62390992e139e23612985b5eee8043939f795683
Akira Shinohara <k017c1067@it-neec.jp>
2019-11-08 22:07:25 +0900

    [new] create contents.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="home">
     <h1>Markdown Sorepponizer</h1>
-    <p>頼む ビジネスメール文化滅びてくれ</p>
+    <p>{{ message }}</p>
+
+    <div id="editor" class="col text_left">
+      <CodeMirror v-model="text" width="600px" :options="cmOption" />
+    </div>
+    <div id="preview" class="col text_left">
+      <textarea v-model="text" readonly />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { codemirror } from "vue-codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/mode/markdown/markdown";
+import "codemirror/theme/monokai.css";
 
 export default {
   name: "home",
-  components: {}
+  components: {
+    CodeMirror: codemirror
+  },
+  data() {
+    return {
+      text: "",
+      message: "いうほどそれっぽいか？",
+      // この辺をユーザが任意で設定できるユーティリティを提供する
+      cmOption: {
+        connect: "align",
+        mode: "text/markdown",
+        theme: "monokai",
+        lineWrapping: true,
+        lineNumbers: true,
+        autoFocus: true,
+        collapseIdentical: false,
+        highlightDifferences: true
+      }
+    };
+  }
 };
 </script>
