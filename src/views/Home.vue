39ded7432bad8837d2d356b1c55c7a165f41fab2
Akira Shinohara <k017c1067@it-neec.jp>
2019-11-08 20:18:58 +0900

    [change] clean Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Markdown Sorepponizer</h1>
+    <p>頼む ビジネスメール文化滅びてくれ</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
